3af67600f6cf397028120d867a709d08c697f1f9
Cedric Yin <ysq2023@gmail.com>
2024-02-28 16:07:06 +0800

    fixed fizzbuzz(num)

--- lab00/code.py
+++ lab00/code.py
@@ -5,12 +5,16 @@ def get_airspeed_velocity_of(unladen_swallow):
     return # redacted
 
 def fizzbuzz(num):
-  if str(num) in ["15"]:
-    print(f"{num}: fizzbuzz")
-  elif str(num) in ["3", "6", "9", "12", "15", "18"]:
-    print(f"{num}: fizz")
-  elif str(num) in ["5", "10", "15"]:
-    print(f"{num}: buzz")
+  # if str(num) in ["15"]:
+  #   print(f"{num}: fizzbuzz")
+  # elif str(num) in ["3", "6", "9", "12", "15", "18"]:
+  #   print(f"{num}: fizz")
+  # elif str(num) in ["5", "10", "15"]:
+  #   print(f"{num}: buzz")
+  if num % 3 == 0:
+    print("num: fizz")
+  elif num % 5 == 0:
+    print("num: buzz")
 
 for i in range(1, 20):
   fizzbuzz(i)
