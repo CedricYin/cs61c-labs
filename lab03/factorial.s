c5306bcbe1ad2b691c759d5ff33ef16266d1b375
Cedric Yin <ysq2023@gmail.com>
2024-03-05 19:06:46 +0800

    finish lab03

--- lab03/factorial.s
+++ lab03/factorial.s
@@ -22,3 +22,18 @@ main:
 
 factorial:
     # YOUR CODE HERE
+    addi sp, sp, -4
+    sw s0, 0(sp)
+    addi s0, x0, 1  # sum = 1
+    add t0, x0, a0  # n
+    loop:
+        beq t0, x0, end
+        mul s0, s0, t0
+        addi t0, t0, -1  # n--
+        jal x0, loop
+    end:
+        add a0, x0, s0
+        lw s0, 0(sp)
+        addi sp, sp, 4
+        jr ra
+    
